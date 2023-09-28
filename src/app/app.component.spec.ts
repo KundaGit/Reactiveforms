6fb88ca6f57929ee22a1fff2184ac41f249e6483
Kundan Rajak <kundangit0697@gmail.com>
2023-09-28 16:26:56 +0530

    Forms-push

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule,ReactiveFormsModule],
     declarations: [AppComponent]
   }));
 
